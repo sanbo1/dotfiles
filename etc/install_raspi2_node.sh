f520c2a6435c839106d2190a8cdc01e862537d2b
sanbo1 <sanbo.dev@gmail.com>
2015-11-22 17:24:28 +0900

    add node.js/npm upgrade, yeoman install

--- etc/install_raspi2_node.sh
+++ etc/install_raspi2_node.sh
@@ -4,11 +4,33 @@
 if [ $(which node | wc -l) -eq 0 ]; then
     wget http://node-arm.herokuapp.com/node_latest_armhf.deb
     sudo dpkg -i node_latest_armhf.deb
+    rm -f ./node_latest_armhf.deb
+fi
+
+
+# upgrade node.js & npm
+if [ $(which npm | wc -l) -eq 1 ]; then
+    # npm upgrade
+    sudo npm install -g npm
+
+    # node.js upgrade
+    sudo npm install -g n
+    sudo n latest
 fi
 
 # install check
 node -v
 npm --version
 
-rm -f ./node_latest_armhf.deb
+
+# install yeaman
+if [ $(which npm | wc -l) -eq 1 ]; then
+    sudo npm install -g yo grunt-cli bower
+fi
+
+
+# install angular-fullstack
+if [ $(which npm | wc -l) -eq 1 ]; then
+    sudo npm install -g generator-angular-fullstack
+fi
 
